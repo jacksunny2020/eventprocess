78ad5719a3c8c1e9033438cef0ef0efe3ba0cec4
jacksunny2020 <jacksunny2020@126.com>
2017-08-08 11:35:03 +0800

    Add files via upload

--- src/TreeNodeContract.php
+++ src/TreeNodeContract.php
@@ -15,13 +15,35 @@ namespace Jacksunny\EventProcess;
  */
 interface TreeNodeContract {
 
-    function getId();
+    /**
+     * 返回实体在组织树上的编号，通常会在实体本身编号加上一个起始值作为最终编号
+     * 如果实体是 platform 则返回的编号为 platform->id + 100000000
+     * 如果实体是 brand 则返回的编号为 brand->id + 200000000
+     * 如果实体是 company 则返回的编号为 company->id + 300000000
+     * 如果实体是 branch 则返回的编号为 branch->id + 400000000
+     * 如果实体是 user 则返回的编号为 user->id + 600000000
+     * 如果实体是 merchant 则返回的编号为 merchant->id + 800000000
+     * 如果实体是 agent 则返回的编号为 agent->id + 900000000
+     */
+    function getTreeNodeId();
 
     function getEntityName();
 
     function getNodeType();
 
-    function getParentId();
+    /**
+     * 返回实体的父实体在组织树上的编号，通常会在父实体本身编号加上一个起始值作为最终编号
+     * 如果父实体是 platform 则返回的编号为 platform_id + 100000000
+     * 如果父实体是 brand 则返回的编号为 brand_id + 200000000
+     * 如果父实体是 company 则返回的编号为 company_id + 300000000
+     * 如果父实体是 branch 则返回的编号为 branch_id + 400000000
+     * 如果父实体是 user 则返回的编号为 user_id + 600000000
+     * 如果父实体是 merchant 则返回的编号为 merchant_id + 800000000
+     * 如果父实体是 agent 则返回的编号为 agent_id + 900000000
+     */
+    function getTreeNodeParentId();
+
+    function getTreeNodeParentType();
 
     function getEntityStatus();
 }
